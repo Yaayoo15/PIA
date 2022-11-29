8b9d929467e2675638e1744d7c5ed7365ffabc53
Yaayoo15 <105295599+Yaayoo15@users.noreply.github.com>
2022-11-29 01:27:22 -0600

    Create bro.sh

---
+++ Scripting en Bash/bro.sh
@@ -0,0 +1,9 @@
+#Nombre: Angel Yahir Flores Garvalena
+#Matricula: 2006659
+
+#!/bin/bash
+# Script <bro.sh> 
+# 27/09/2022 - Angel Yahir Flores Garvalena 
+#
+read -p "Escribe tu nombre: " nombre
+echo "Hola, $nombre. Seamos amigos!"
