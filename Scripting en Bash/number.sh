9860ae07ee4f6bc0e5f7d911fd3f93cd81c3529e
Yaayoo15 <105295599+Yaayoo15@users.noreply.github.com>
2022-11-29 01:28:09 -0600

    Create number.sh

---
+++ Scripting en Bash/number.sh
@@ -0,0 +1,17 @@
+#Nombre: Angel Yahir Flores Garvalena
+#Matricula: 2006659
+
+#!/bin/bash
+# Script <number.sh> 
+# 27/09/2022 - Angel Yahir Flores Garvalena 
+# 
+
+# Leer 3 numeros asignados a 3 variables
+read -p "Proporciona un numero para variable 1: " n1
+read -p "Proporciona un numero para variable 2: " n2
+read -p "Proporciona un numero para variable 3: " n3
+#
+# Despliega los 3 numeros proporcionados por el usuario
+echo "Numero 1 - $n1"
+echo "Numero 2 - $n2"
+echo "Numero 3 - $n3"
