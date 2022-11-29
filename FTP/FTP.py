707e03a75926957bfdb52ac9ace6840ffbadfc73
Yaayoo15 <105295599+Yaayoo15@users.noreply.github.com>
2022-11-29 01:46:33 -0600

    Create FTP.py

---
+++ FTP/FTP.py
@@ -0,0 +1,33 @@
+#Nombre: Angel Yahir Flores Garvalena
+#Matricula: 2006659
+#Fecha: 25/Oct/22
+
+#Script para proceso de transferencia de FTP
+
+#Importando módulo ftp 
+from ftplib import FTP
+
+#Nos conectaremos al servidor, utilizando la IP de nuestra máquina.
+ftp = FTP('192.168.162.133')
+
+#Ingresamos con las credenciales preestablecidas en la creación del usuario;
+# 'yayoo' en mi caso
+ftp.login('yayoo','2006659')
+
+#A continuación nos cambiaremos al directorio upload (/ftp/upload) 
+#para poder insertar aquí el archivo ADVERTENCIA.txt
+ftp.cwd('upload')
+
+#Haciendo uso del método ftp.retrlines (equivalente a 'ls' en BASH) 
+#enlistaremos lo que hay en la ruta en que estamos
+ftp.retrlines('LIST')
+
+#Ahora procedemos a depositar en dicha ruta el archivo ADVERTENCIA.txt;
+#En nuestro caso será un archivo modificado, sin embargo, cumple con su funcionalidad de 
+#insertar cualquier archivo por medio de este script
+ftp.storlines('STOR ADVERTENCIA.txt',open('ADVERTENCIA.txt','rb'))
+
+#Por último y para finalizar la transferencia utilizamos ftp.quit(), que sería
+#equiparable a 'bye' en ftp, para de esta manera dar por concluida la conexión entre 
+#Python y FTP
+ftp.quit()
