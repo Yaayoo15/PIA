65ced7fa1c0cdacb04b723237a07665c9b3e1a78
Yaayoo15 <105295599+Yaayoo15@users.noreply.github.com>
2022-11-29 01:26:09 -0600

    Update welcome.sh

--- Scripting en Bash/welcome.sh
+++ Scripting en Bash/welcome.sh
@@ -1,12 +1,12 @@
 #Nombre: Angel Yahir Flores Garvalena
 #Matricula: 2006659
-#Fecha: 27/09/22
 
 #!/bin/bash
-#
 # Script welcome.sh
+# 27/09/2022 - Angel Yahir Flores Garvalena
 #
 echo "Hola ${LOGNAME}"
 echo "Hoy es $(date)"
 echo "Usuarios actuales conectados y sus procesos:"
 w
+
