452d62f9dd1d9eb8399dea80929c17c12a8c539d
Yaayoo15 <105295599+Yaayoo15@users.noreply.github.com>
2022-11-29 01:23:59 -0600

    Create welcome.sh

---
+++ Scripting en Bash/welcome.sh
@@ -0,0 +1,12 @@
+#Nombre: Angel Yahir Flores Garvalena
+#Matricula: 2006659
+#Fecha: 27/09/22
+
+#!/bin/bash
+#
+# Script welcome.sh
+#
+echo "Hola ${LOGNAME}"
+echo "Hoy es $(date)"
+echo "Usuarios actuales conectados y sus procesos:"
+w
