1c3d91fb68912d0fa2c083ba1760865a85cebf90
Yaayoo15 <105295599+Yaayoo15@users.noreply.github.com>
2022-11-29 01:10:58 -0600

    Update Pokemon1.py

---
+++ Manejo de APIs/Pokemon1.py
@@ -0,0 +1,17 @@
+from unittest import result
+import requests
+#
+#Nombre: Angel Yahir Flores Garvalena
+#Matricula: 2006659
+if __name__ == '__main__':
+    url = 'https://pokeapi.co/api/v2/pokemon-form/'
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        payload = response.json()
+        results = payload.get('results',[])
+
+        if results:
+            for pokemon in results:
+                name = pokemon['name']
+                print(name)
